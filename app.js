765fb43c1db325e82b3e6045c97d091839535360
Ricky Cordero <8922867+RickyCordero@users.noreply.github.com>
2019-03-02 16:23:52 -0500

    First commit, working login

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+
+const app = express();
+
+// Passport config
+require('./config/passport')(passport);
+
+// DB Config
+// const db = require('./config/key').MongoURI;
+const URI = "mongodb://localhost:27017/passport-app";
+
+mongoose.connect(URI, { useNewUrlParser: true })
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Bodyparser
+// allows us to get data from our form using request.body
+app.use(express.urlencoded({ extended: false }));
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+// allows us to use request.flash
+app.use(flash());
+
+// Global Vars
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
+
+// Routes
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
